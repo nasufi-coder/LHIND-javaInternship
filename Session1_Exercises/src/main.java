0fdf7e249862c4d08a0d1c1070d9087ff8a62bb1
Patrik <patrik.nasufi@gmail.com>
2022-06-10 08:31:30 -0700

    lastChange

---
+++ Session1_Exercises/src/main.java
@@ -0,0 +1,10 @@
+public class main extends students{
+    public static void main(String[] args) {
+        students s1= new students("patrik","nasufi");
+        s1.setName("test");
+        System.out.println(s1.getName());
+    }
+    public static void test(int test){
+
+    }
+}
